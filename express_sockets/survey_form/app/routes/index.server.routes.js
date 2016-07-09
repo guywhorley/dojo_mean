f2e08c9fbb0db879dfed6353671e3c522d8b42d5
Guy Whorley <guy.whorley@gmail.com>
2016-07-09 11:07:50 -0700

    removing node modules.

--- express_sockets/survey_form/app/routes/index.server.routes.js
+++ express_sockets/survey_form/app/routes/index.server.routes.js
@@ -12,11 +12,6 @@ module.exports = function(app) {
   app.post('/surveys', function(req, res) {
     console.log("POST DATA", req.body);
     return res.render('show', { data: req.body});
-
-      // name: req.body.name,
-      // location: req.body.location,
-      // language: req.body.language,
-      // comment: req.body.comment,});
-   });
+  });
 
 };
